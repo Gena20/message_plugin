f334ef7cf1bf0da3aaac1d4f803595d4fcac5248
Gena <peryatin@echo-company.ru>
2020-11-05 11:55:32 +0500

    added mustache templates

--- manage.php
+++ manage.php
@@ -21,5 +21,18 @@
  * @var stdClass $plugin
  */
 
-defined('MOODLE_INTERNAL') || die();
+require_once(__DIR__ . '/../../config.php');
+$PAGE->set_url(new moodle_url('/local/message/manage.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Manage messages');
+
+echo $OUTPUT->header();
+
+$templatecontext = (object)[
+        'texttodisplay' => 'here is example'
+];
+
+echo $OUTPUT->render_from_template('local_message/manage', $templatecontext);
+
+echo $OUTPUT->footer();
 

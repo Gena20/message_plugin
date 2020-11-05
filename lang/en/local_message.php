67902b19c2c1f14b109019ae90cd2468b0de8bbb
Gena <peryatin@echo-company.ru>
2020-11-05 12:45:11 +0500

    added lang

---
+++ lang/en/local_message.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_students
+ * @category    string
+ * @copyright   2020 Your Name <you@example.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Message';
+$string['modulename'] = 'Message';
+$string['modulenameplural'] = 'Messages';
+$string['pluginadministration'] = 'Message settings';
+$string['manageheader'] = 'List of messages';
+$string['messagetextinput'] = 'Message text';
+$string['messagetypeselect'] = 'Message type';
